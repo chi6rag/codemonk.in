286b5bc9b38666b3ec5b70f059bf4002c0ffd89e
Niranjan Paranjape <niranjangp@gmail.com>
2015-05-13 12:15:04 +0530

    Adding deploy

--- config.rb
+++ config.rb
@@ -62,11 +62,17 @@ configure :build do
   # activate :minify_javascript
 
   # Enable cache buster
-  # activate :asset_hash
+  activate :asset_hash
 
   # Use relative URLs
   # activate :relative_assets
 
   # Or use a different image path
   # set :http_prefix, "/Content/images/"
+end
+
+activate :deploy do |deploy|
+  deploy.build_before = true
+  deploy.method = :git
+  deploy.branch = "gh-pages"
 end
